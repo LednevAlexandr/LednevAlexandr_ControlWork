9686aefd5b40236f8ed56036faa1b70428685ecf
alex <you@example.com>
2022-01-02 17:52:37 +0300

    added PrintEven

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -17,5 +17,16 @@ void PrintArray(int[] array)
 
 int[] array = new int[10];
 
+void PrintEven(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2==0 && array[i]!=0) Console.Write($"{array[i],4}");
+        
+    }
+}
+
 CreateArray(array);
 PrintArray(array);
+System.Console.WriteLine();
+PrintEven(array);
