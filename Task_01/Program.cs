1c7819a66d1d21bcf46895cb309b3ef1434707cb
alex <you@example.com>
2022-01-02 17:31:50 +0300

    added PrintArray and CreateArray

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Random rnd = new Random();
+void CreateArray (int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(-10,10);
+}
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i],4}");
+    }
+}
+
+int[] array = new int[10];
+
+CreateArray(array);
+PrintArray(array);
